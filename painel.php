307584639fe5530a8334642a15da4eb5630acbea
Angelo Gustavo <angelogustavo157@gmail.com>
2022-04-28 23:12:48 -0300

    Segundo commit

--- painel.php
+++ painel.php
@@ -1,5 +1,17 @@
 <?php include('comum/header.html'); ?>
 
-<p>Teste</p>
+<h3>Jogos</h3>
+
+<div class="row">
+  <div class="column">
+    <a href="painel.php"><img src="img/crash.png" alt="Jogo 1" style="width:100%"></a>
+  </div>
+  <div class="column">
+    <a href="painel.php"><img src="img/roleta.png" alt="Jogo 2" style="width:100%"></a>
+  </div>
+  <div class="column">
+  	<a href="painel.php"><img src="img/keno.png" alt="Jogo 3" style="width:100%"></a>
+  </div>
+</div>
 
 <?php include('comum/footer.html'); ?>
