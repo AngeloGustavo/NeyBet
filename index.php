33c78e96a2cff166db9315a52be84d030236ee42
Angelo Gustavo <angelogustavo157@gmail.com>
2022-04-28 21:19:55 -0300

    Correção index

--- index.php
+++ index.php
@@ -1,23 +1,5 @@
 <?php include('comum/header.html'); ?>
 
-<h1>Ola, esta e a minha pagina!</h1>
-<p>Fique a vontade corninha!</p>
-<p>Segundo paragrafo!!</p>
-<a href=https://www.google.com">Google</a>
-<hr>
-
-<h1>Deciciu?</h1>
-<p>Nao ne?</p>
-
-<button type="button" onclick="location.href='https://www.google.com'">Click Me!</button>
-
-<button type="button" onclick="location.href='painel.php'">Blaze!</button>
-
-<pre>
-<h2>aaaaaaau
-asdasda
-222222222222
-</h2>
-</pre>
+<button type="button" onclick="location.href='painel.php'"> Receba! NeyBet.com </button>
 
 <?php include('comum/footer.html'); ?>
