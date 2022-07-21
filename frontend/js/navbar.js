3982d372ae8bfc5fd58a58711adb0262b3385574
Neylane <neylanepl@gmail.com>
2022-07-21 02:18:09 -0300

    Frontend da carteira

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,11 +1,5 @@
-// var dinheiroCarteira = 5.00;
-
-// function carteira(){  
-//     document.getElementById("valorCateira").innerHTML = dinheiroCarteira;
-// }
-
 document.write("<div class='topnav' id='myTopnav'>\
 <a class='' href='index.html'><img src='img/logo.png' alt='Jogo 1' style='max-height: 50px; margin: -1px;'></a>\
-<div class='textoCarteira' id='textoCarteira' style='float:right'> Carteira R$&nbsp; <div class='valorCarteira' id='valorCarteira' onload='carteira()'></div></div>\
+<div class='textoCarteira' id='textoCarteira' > Carteira R$&nbsp; <div class='valorCarteira' id='valorCarteira'></div></div>\
 </div>\
 ");
--- frontend/js/navbarUpDir.js
+++ frontend/js/navbarUpDir.js
@@ -1,5 +1,5 @@
 document.write("<div class='topnav' id='myTopnav'>\
 <a class='' href='../index.html'><img src='../img/logo.png' style='max-height: 50px; margin: -1px;'></a>\
-<div class='textoCarteira' id='textoCarteira' style='float: right'> Carteira R$&nbsp; <div class='valorCarteira' id='valorCarteira'></div></div>\
+<div class='textoCarteira' id='textoCarteira'> Carteira R$&nbsp; <div class='valorCarteira' id='valorCarteira'></div></div>\
 </div>\
 ");
